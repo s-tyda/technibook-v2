c9ad201fcd13bfe5ccc1fbbeb3e8084ca2415ac1
s-tyda <styda@huhha.co>
2024-09-18 09:38:05 +0200

    initial message

---
+++ src/main/java/org/technischools/technibook/TechnibookApplication.java
@@ -0,0 +1,13 @@
+package org.technischools.technibook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnibookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnibookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/technischools/technibook/TechnibookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.technischools.technibook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnibookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
